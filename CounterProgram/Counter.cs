9c18bf4f052453154689d8a66f7d5191f90ecb1a
lukababu <luke.iremadze@ucalgary.ca>
2018-01-24 00:13:28 -0700

    Add project files.

---
+++ CounterProgram/Counter.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace CounterProgram
+{
+    public class Counter : INotifyPropertyChanged
+    {
+        private int count = 0;
+
+        
+
+        #region Counter accesors
+        public int Count
+        {
+            get
+            {
+                return this.count;
+            }
+            set
+            {
+                this.count = value;
+                RaisePropertyChanged(new PropertyChangedEventArgs("Count"));
+            }
+        }
+
+        public void IncrementCount()
+        {
+            this.count++;
+        }
+
+        public void DecrementCount()
+        {
+            if (count > 0)
+            {
+                this.count--;
+            }
+        }
+        #endregion
+
+        #region Events
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public virtual void RaisePropertyChanged(PropertyChangedEventArgs e)
+        {
+            PropertyChanged?.Invoke(this, e);
+        }
+
+        #endregion
+
+        public enum FoodType
+        {
+            Meat = 0,
+            Fruit = 1,
+            Vegetable = 2,
+            Other = 3
+        }
+
+        List<FoodType> foodItems = new List<FoodType>();
+
+}
+}
---
+++ CounterProgram/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace CounterProgram
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Counter counter = new Counter();
+        DispatcherTimer timer = new DispatcherTimer();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.incButton.Click += IncButton_Click;
+            this.decButton.Click += DecButton_Click;
+            // Set an interval at 300ms
+            timer.Interval = TimeSpan.FromMilliseconds(300);
+ 
+            timer.Tick += Timer_Tick;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            this.counter.IncrementCount();
+            this.countLabel.Content = this.counter.Count.ToString();
+            this.timer.Start();
+        }
+
+        private void DecButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.counter.DecrementCount();
+            this.countLabel.Content = this.counter.Count.ToString();
+        }
+
+        private void IncButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.counter.IncrementCount();
+            this.countLabel.Content = this.counter.Count.ToString();
+        }
+
+        private void stopTimer_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
